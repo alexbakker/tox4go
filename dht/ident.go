671c88958daa7e01f0df5ec25eee3ff6a491ccfd
Alexander Bakker <ab@alexbakker.me>
2024-01-02 23:41:51 +0100

    Rename dht.Ident to dht.Identity

--- dht/ident.go
+++ dht/ident.go
@@ -7,20 +7,20 @@ import (
 	"github.com/alexbakker/tox4go/transport"
 )
 
-// Ident represents a DHT identity.
-type Ident struct {
+// Identity represents a DHT identity.
+type Identity struct {
 	PublicKey *[crypto.PublicKeySize]byte
 	SecretKey *[crypto.SecretKeySize]byte
 }
 
-// NewIdent creates a new DHT identity and generates a new keypair for it.
-func NewIdent() (*Ident, error) {
+// NewIdentity creates a new DHT identity and generates a new keypair for it.
+func NewIdentity() (*Identity, error) {
 	publicKey, secretKey, err := crypto.GenerateKeyPair()
 	if err != nil {
 		return nil, err
 	}
 
-	inst := &Ident{
+	inst := &Identity{
 		PublicKey: publicKey,
 		SecretKey: secretKey,
 	}
@@ -29,7 +29,7 @@ func NewIdent() (*Ident, error) {
 }
 
 // EncryptPacket encrypts the given packet.
-func (i *Ident) EncryptPacket(packet transport.Packet, publicKey *[crypto.PublicKeySize]byte) (*Packet, error) {
+func (i *Identity) EncryptPacket(packet transport.Packet, publicKey *[crypto.PublicKeySize]byte) (*Packet, error) {
 	base := Packet{}
 	base.Type = packet.ID()
 	base.SenderPublicKey = i.PublicKey
@@ -51,7 +51,7 @@ func (i *Ident) EncryptPacket(packet transport.Packet, publicKey *[crypto.Public
 }
 
 // DecryptPacket decrypts the given packet.
-func (i *Ident) DecryptPacket(p *Packet) (transport.Packet, error) {
+func (i *Identity) DecryptPacket(p *Packet) (transport.Packet, error) {
 	var tPacket transport.Packet
 
 	switch p.Type {
@@ -81,7 +81,7 @@ func (i *Ident) DecryptPacket(p *Packet) (transport.Packet, error) {
 }
 
 // EncryptBlob encrypts the given slice of data.
-func (i *Ident) EncryptBlob(data []byte, publicKey *[crypto.PublicKeySize]byte) ([]byte, *[crypto.NonceSize]byte, error) {
+func (i *Identity) EncryptBlob(data []byte, publicKey *[crypto.PublicKeySize]byte) ([]byte, *[crypto.NonceSize]byte, error) {
 	sharedKey := crypto.PrecomputeKey(publicKey, i.SecretKey)
 	encryptedPayload, nonce, err := crypto.Encrypt(data, sharedKey)
 	if err != nil {
@@ -92,7 +92,7 @@ func (i *Ident) EncryptBlob(data []byte, publicKey *[crypto.PublicKeySize]byte) 
 }
 
 // DecryptBlob decrypts the given slice of data.
-func (i *Ident) DecryptBlob(data []byte, publicKey *[crypto.PublicKeySize]byte, nonce *[crypto.NonceSize]byte) ([]byte, error) {
+func (i *Identity) DecryptBlob(data []byte, publicKey *[crypto.PublicKeySize]byte, nonce *[crypto.NonceSize]byte) ([]byte, error) {
 	sharedKey := crypto.PrecomputeKey(publicKey, i.SecretKey)
 	decryptedData, err := crypto.Decrypt(data, sharedKey, nonce)
 	if err != nil {
