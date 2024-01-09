2128270d15fb76f506c8d9758227e212532a6e76
Alexander Bakker <ab@alexbakker.me>
2024-01-09 21:58:04 +0100

    Rename ping collection to set

--- dht/ping/set.go
+++ dht/ping/set.go
@@ -12,25 +12,25 @@ type pingKey struct {
 	PublicKey dht.PublicKey
 }
 
-type Collection struct {
+type Set struct {
 	pings   []*Ping
 	pingMap map[pingKey]*Ping
 	timeout time.Duration
 }
 
-func NewCollection(timeout time.Duration) *Collection {
-	return &Collection{
+func NewSet(timeout time.Duration) *Set {
+	return &Set{
 		pingMap: make(map[pingKey]*Ping),
 		timeout: timeout,
 	}
 }
 
-func (c *Collection) Add(p *Ping) error {
+func (c *Set) Add(p *Ping) error {
 	c.ClearExpired()
 
 	key := pingKey{PublicKey: *p.PublicKey, ID: p.ID}
 	if _, ok := c.pingMap[key]; ok {
-		return fmt.Errorf("ping id already in collection: %d", p.ID)
+		return fmt.Errorf("ping id already in set: %d", p.ID)
 	}
 
 	c.pingMap[key] = p
@@ -38,7 +38,7 @@ func (c *Collection) Add(p *Ping) error {
 	return nil
 }
 
-func (c *Collection) AddNew(publicKey *dht.PublicKey) (*Ping, error) {
+func (c *Set) AddNew(publicKey *dht.PublicKey) (*Ping, error) {
 	p, err := New(publicKey)
 	if err != nil {
 		return nil, err
@@ -51,18 +51,17 @@ func (c *Collection) AddNew(publicKey *dht.PublicKey) (*Ping, error) {
 	return p, nil
 }
 
-// Pop tries to find the given ping ID and public key in the collection of
-// pings. If it is found, it is removed from the collection and returned. If it
-// is not found, an error is returned. Expiry can also be the reason that the
-// given ping ID is not found, but this will not be reported as a separate
-// error.
-func (c *Collection) Pop(publicKey *dht.PublicKey, id uint64) (*Ping, error) {
+// Pop tries to find the given ping ID and public key in the set of pings. If it
+// is found, it is removed from the set and returned. If it is not found, an
+// error is returned. Expiry can also be the reason that the given ping ID is
+// not found, but this will not be reported as a separate error.
+func (c *Set) Pop(publicKey *dht.PublicKey, id uint64) (*Ping, error) {
 	c.ClearExpired()
 
 	key := pingKey{PublicKey: *publicKey, ID: id}
 	p, ok := c.pingMap[key]
 	if !ok {
-		return nil, fmt.Errorf("ping id not in collection: %d", id)
+		return nil, fmt.Errorf("ping id not in set: %d", id)
 	}
 
 	// Delete the ping from the map immediately, but wait for expiry before
@@ -72,7 +71,7 @@ func (c *Collection) Pop(publicKey *dht.PublicKey, id uint64) (*Ping, error) {
 	return p, nil
 }
 
-func (c *Collection) ClearExpired() {
+func (c *Set) ClearExpired() {
 	// Iterate through the time-sorted list of pings and delete any pings from
 	// the map that have expired
 	var deli int
